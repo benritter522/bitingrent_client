b4960fac2d60f7336acff69dcffe2f1154c45a66
Ben <Ben@Bens-MacBook-Pro.local>
2021-02-02 18:51:50 -0500

    Added projects to home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,47 @@
-// import React from 'react';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+    const [projects, setProjects] = useState([]);
+
+    // Read
+    const fetchProjects = async () => {
+        try {
+            const response = await fetch('http://bitingrent-backend.herokuapp.com/');
+            const data = await response.json();
+            setProjects(data);
+        } catch(error) {
+            console.error(error); // {msg: error.message} ??
+        }
+    }
+
+    useEffect(() => {
+        fetchProjects();
+    }, [])
     return(
         <div>
-            <h1>Welcome! Please go ahead and check out my music and plants.</h1>
+            <h2>Welcome! Please, check out my projects below, or go ahead and check out my music and plants. </h2>
+            <div className="indexProjects">
+                {
+                    projects.map((project, index) => {
+                        return(
+                            <div 
+                                className="indexSingleProject"
+                                key={index}
+                            >
+                                <h2 className="indexProjectTitle">{project.title}</h2>
+                                <img src={project.image} alt={`Project ${project.title}`} className="indexProjectImg"/>
+                                {
+                                    project.links.map((projectLink, index) => {
+                                        return(
+                                            <a href="projectLink">Check it out!</a>
+                                        )
+                                    })
+                                }
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
